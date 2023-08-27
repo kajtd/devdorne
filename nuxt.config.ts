a23238deaae4ba94325d1083aea2cbc9bece71f9
Kajetan Domagala <kajtekd8@gmail.com>
2023-08-27 20:54:00 +0200

    build: add tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,29 @@ export default defineNuxtConfig({
   content: {
     // https://content.nuxtjs.org/api/configuration
   },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com',
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Kufam:wght@400;500;600;700;800;900&display=swap',
+        },
+      ],
+    },
+  },
 });
