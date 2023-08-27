d8f4542b80a9ead9581b32d17574ef62de0e357b
Kajetan Domagala <kajtekd8@gmail.com>
2023-08-27 21:07:04 +0200

    feat: add welcome section

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,12 @@
-<template>hello its home</template>
+<template>
+  <main class="px-3 md:px-12 w-full max-w-7xl mx-auto">
+    <section class="flex flex-col md:flex-row items-center">
+      <h1
+        class="text-center md:text-left text-5xl lg:text-7xl leading-tight font-medium max-w-lg mt-24"
+      >
+        Tips, Tutorials & Ideas.
+      </h1>
+      <WelcomeAnimation />
+    </section>
+  </main>
+</template>
