62c8a9639139b5bf475e66f95093801fd0db23be
Kajetan Domagala <kajtekd8@gmail.com>
2023-08-28 16:15:23 +0200

    feat: add home sidebar with tags and total posts number

---
+++ components/HomeSidebar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="flex flex-col gap-12">
+    <div
+      class="border-[3px] border-black rounded-3xl pb-4 max-h-[500px] bg-white"
+    >
+      <div class="mt-12 relative">
+        <span
+          class="bg-primary rounded-3xl border-[3px] border-black px-20 py-4 absolute -top-8 left-6 font-bold text-2xl"
+        >
+          Tags
+        </span>
+      </div>
+      <div
+        class="border-t-[3px] border-black pt-16 px-6 flex flex-wrap gap-3 scroll h-[calc(100%-48px)]"
+      >
+        <ArticleTag />
+        <ArticleTag />
+        <ArticleTag />
+        <ArticleTag />
+        <ArticleTag />
+        <ArticleTag />
+        <ArticleTag />
+        <ArticleTag />
+        <ArticleTag />
+        <ArticleTag />
+        <ArticleTag />
+      </div>
+    </div>
+    <div class="border-[3px] border-black rounded-3xl h-64 bg-white">
+      <div class="mt-12 relative">
+        <span
+          class="bg-primary rounded-3xl border-[3px] border-black px-12 py-4 absolute -top-8 left-6 font-bold text-2xl"
+        >
+          Total Posts
+        </span>
+      </div>
+      <div
+        class="text-7xl font-bold h-full w-full border-t-[3px] border-black grid place-items-center"
+      >
+        41
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,21 @@
 <template>
-  <main class="px-3 md:px-12 w-full max-w-7xl mx-auto">
-    <section class="flex flex-col md:flex-row items-center">
+  <main class="flex-1 px-3 md:px-12 w-full max-w-7xl mx-auto">
+    <section class="flex flex-col md:flex-row items-center xl:mb-24">
       <h1
         class="text-center md:text-left text-5xl lg:text-7xl leading-tight font-medium max-w-lg mt-24"
       >
         Tips, Tutorials & Ideas.
       </h1>
       <WelcomeAnimation />
+    </section>
+    <section
+      class="w-full grid grid-cols-1 xl:grid-cols-[5fr_2fr] py-12 gap-10"
+    >
+      <div class="flex flex-col gap-5">
+        <ArticleCard />
+        <ArticleCard />
+      </div>
+      <HomeSidebar />
     </section>
   </main>
 </template>
