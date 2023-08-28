e57eebbbe813c55915fad7cf15767c6f5989cb02
Kajetan Domagala <kajtekd8@gmail.com>
2023-08-28 16:14:28 +0200

    feat: add mock article card and mock tags

---
+++ components/ArticleCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <NuxtLink to="/article">
+    <article
+      class="bg-white hover:shadow-[3px_3px_1px_black] transition-shadow border-[3px] border-black rounded-3xl flex flex-col-reverse xl:flex-row items-center p-4"
+    >
+      <div
+        class="flex flex-col justify-between md:pl-5 pt-5 pb-2 md:mr-10 gap-5 md:gap-8"
+      >
+        <div class="flex items-center gap-2">
+          <ArticleTag />
+          <ArticleTag />
+        </div>
+        <h2 class="font-medium text-3xl md:text-4xl">
+          Top Tips for Learning HTML: A Comprehensive Guide
+        </h2>
+        <p class="font-medium">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+          do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat...
+        </p>
+        <div
+          class="flex items-center justify-between text-sm md:text-base"
+        >
+          <span class="font-medium">12 September 2023</span>
+          <div class="flex gap-1 md:gap-3 items-center">
+            <img
+              src="https://www.kajetandomagala.com/content/images/size/w300/2023/04/kajetan_avatar2.jpg"
+              alt="kajetan"
+              class="md:w-12 w-10 h-10 md:h-12 rounded-full border-purple border-[3px]"
+            />
+            <span class="font-medium">Kajetan Domagała</span>
+          </div>
+        </div>
+      </div>
+      <img
+        src="https://www.kajetandomagala.com/content/images/2023/07/sunset-neon-city.webp"
+        class="h-full max-h-[500px] rounded-3xl w-full"
+        alt=""
+      />
+    </article>
+  </NuxtLink>
+</template>
---
+++ components/ArticleTag.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const colors = [
+  'bg-red-500',
+  'bg-purple-500',
+  'bg-yellow-200',
+  'bg-blue-500',
+  'bg-pink-500',
+  'bg-orange-500',
+  'bg-green-500',
+  'bg-[#A8FFD5]',
+  'bg-[#A8BBFF]',
+];
+const tags = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'Nuxt',
+  'Vue',
+  'UX',
+];
+</script>
+
+<template>
+  <div
+    class="border-[3px] border-black rounded-full p-2 md:px-4 md:py-3 h-8 md:h-12 gap-5 flex justify-between"
+  >
+    <span class="md:text-base text-xs">{{
+      tags[Math.floor(Math.random() * tags.length)]
+    }}</span>
+    <div
+      class="border-[3px] border-black h-3 w-3 md:h-5 md:w-5 rounded-full"
+      :class="colors[Math.floor(Math.random() * colors.length)]"
+    ></div>
+  </div>
+</template>
