13745770b717dcafadf8cedc680caec0772f85d4
Kajetan Domagala <kajtekd8@gmail.com>
2023-08-29 10:22:34 +0200

    feat: add contact page

---
+++ components/ContactForm.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+</script>
+
+<template>
+  <div
+    class="mt-5 p-3 md:p-8 rounded-lg max-w-2xl relative flex flex-col py-12 md:py-24 w-full"
+  >
+    <h3 class="text-4xl font-bold mb-2">
+      Say Hello<span class="text-6xl">.</span>
+    </h3>
+    <form
+      class="flex flex-col w-full relative"
+      action="https://formsubmit.co/kajetan.domagala00@gmail.com"
+      method="POST"
+    >
+      <input
+        type="email"
+        id="email"
+        name="email"
+        required
+        placeholder="Your email..."
+        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+        class="p-3 min-h-[66px] border-[3px] border-black pl-6 w-full bg-white rounded-xl outline-none focus:shadow-[3px_3px_1px_black]"
+      />
+      <input
+        type="name"
+        id="name"
+        name="name"
+        required
+        placeholder="Your name..."
+        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+        class="p-3 min-h-[66px] border-[3px] border-black pl-6 w-full bg-white rounded-xl mt-4 outline-none focus:shadow-[3px_3px_1px_black]"
+      />
+      <textarea
+        id="message"
+        name="message"
+        rows="8"
+        required
+        maxlength="500"
+        placeholder="Your message..."
+        class="p-3 min-h-[66px] border-[3px] border-black pl-6 w-full bg-white mt-4 rounded-xl outline-none focus:shadow-[3px_3px_1px_black]"
+      ></textarea>
+      <input type="hidden" name="_template" value="table" />
+      <input type="text" name="_honey" class="hidden" />
+      <AppButton
+        class="mt-5 h-16 text-xl font-medium ml-auto px-8 gap-1"
+        type="submit"
+      >
+        <span class="pt-1">Contact</span>
+        <Icon icon="mingcute:send-fill" class="w-8 h-8" />
+      </AppButton>
+    </form>
+  </div>
+</template>
---
+++ pages/contact.vue
@@ -0,0 +1,34 @@
+<template>
+  <main class="flex-1 px-3 md:px-12 w-full max-w-7xl mx-auto">
+    <section
+      class="flex flex-col md:flex-row items-center justify-center gap-10 md:gap-24 xl:justify-between my-12 xl:my-24"
+    >
+      <h1
+        class="text-center md:text-left text-5xl lg:text-7xl leading-tight font-medium max-w-lg mt-24"
+      >
+        Contact
+      </h1>
+      <img
+        src="./../assets/images/memphis-folder.png"
+        alt="folder"
+        class="h-48 xl:h-96"
+      />
+    </section>
+    <section
+      class="w-full flex flex-col xl:flex-row items-center justify-between xl:py-12 gap-10 mt-16"
+    >
+      <div class="flex relative mb-10 md:mb-0">
+        <p class="font-medium text-lg xl:text-2xl max-w-xs">
+          If there is anything you want to ask us about, feel free to
+          use the contact form.
+        </p>
+        <img
+          src="./../assets/images/memphis-arrow.png"
+          alt="red right arrow"
+          class="absolute h-24 rotate-90 top-24 left-5 xl:left-auto xl:top-auto xl:bottom-12 xl:-right-48 xl:h-40 xl:rotate-12"
+        />
+      </div>
+      <ContactForm />
+    </section>
+  </main>
+</template>
