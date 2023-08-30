78f2ff11bde0cb0fac0d6fcf840a73dcbb7bf635
Kajetan Domagala <kajtekd8@gmail.com>
2023-08-30 13:42:13 +0200

    fix: fix article tags

--- components/ArticleTag.vue
+++ components/ArticleTag.vue
@@ -1,36 +1,31 @@
 <script setup lang="ts">
-const colors = [
-  'bg-red-500',
-  'bg-purple-500',
-  'bg-yellow-200',
-  'bg-blue-500',
-  'bg-pink-500',
-  'bg-orange-500',
-  'bg-green-500',
-  'bg-[#A8FFD5]',
-  'bg-[#A8BBFF]',
-];
-const tags = [
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'TypeScript',
-  'Nuxt',
-  'Vue',
-  'UX',
-];
+defineProps<{
+  text: string;
+}>();
+
+const tags = ref<Record<string, string>>({
+  JavaScript: 'bg-[#A8BBFF]',
+  HTML: 'bg-[#A8FFD5]',
+  CSS: 'bg-yellow-500',
+  UI: 'bg-orange-500',
+  UX: 'bg-green-500',
+  TypeScript: 'bg-red-500',
+  Nuxt: 'bg-purple-500',
+  Git: 'bg-blue-500',
+});
 </script>
 
 <template>
-  <div
+  <NuxtLink
+    :to="`/tags/${text}`"
     class="border-[3px] border-black rounded-full p-2 md:px-4 md:py-3 h-8 md:h-12 gap-5 flex justify-between"
   >
-    <span class="md:text-base text-xs font-medium">{{
-      tags[Math.floor(Math.random() * tags.length)]
-    }}</span>
+    <span class="md:text-base text-xs font-medium">
+      {{ text }}
+    </span>
     <div
       class="border-[3px] border-black h-3 w-3 md:h-5 md:w-5 rounded-full"
-      :class="colors[Math.floor(Math.random() * colors.length)]"
+      :class="tags[text]"
     ></div>
-  </div>
+  </NuxtLink>
 </template>
