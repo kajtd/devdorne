bd774ccb99bb3fc9ceae7e76ff506d48af8559b2
Kajetan Domagala <kajtekd8@gmail.com>
2023-08-30 13:41:30 +0200

    fix: remove pseudoelement from logo

--- components/AppLogo.vue
+++ components/AppLogo.vue
@@ -1,9 +1,7 @@
 <template>
   <NuxtLink to="/">
-    <span
-      class="text-2xl md:text-4xl z-[1] after:z-[-1] font-medium relative after:bg-primary md:after:h-4 after:w-full after:absolute after:h-3 after:top-4 md:after:top-5 after:left-0"
-    >
-      devdorne
+    <span class="text-2xl md:text-4xl z-[1] after:z-[-1] font-medium">
+      devdorne.
     </span>
   </NuxtLink>
 </template>
