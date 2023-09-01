4a3244ba718df5e27a62a90358a8decbe0192cd4
Kajetan Domagala <kajtekd8@gmail.com>
2023-09-01 08:54:50 +0200

    fix: add missing lang property to html

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,9 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: 'devdorne.',
+      htmlAttrs: {
+        lang: 'en',
+      },
       meta: [
         {
           name: 'description',
